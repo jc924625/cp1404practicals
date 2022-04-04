c4f8ed8fa5685c2e43b2fe916316c1c47b66b001
johns <johns.bijoy@my.jcu.edu.au>
2022-04-04 18:43:18 +0800

    added prac files

---
+++ prac_02/randoms.py
@@ -0,0 +1,9 @@
+import random
+print(random.randint(5, 20))  # line 1
+#the smallest number is 5 and the largest number is 19
+print(random.randrange(3, 10, 2))  # line 2
+#the smallest number is 3 and largest number is 9,no line 2 will not produce a 4.
+print(random.uniform(2.5, 5.5))  # line 3
+#the smallest number is 2.5 and the largest number is 5.4
+print(random.randint(1,101))
+#to produce a random number between 1 and 100 inclusive.
